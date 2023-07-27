1f184cd9de63bf8eaf720cea98446a4bec66bb76
jkm1cob <kathiresan.manjunath@in.bosch.com>
2023-07-27 12:32:17 +0530

    parameterized test

--- test/valueParameterizationTest.cpp
+++ test/valueParameterizationTest.cpp
@@ -10,7 +10,33 @@ int divFun(int numerator, int denominator)
     return numerator / denominator;
 }
 
-TEST(DivFunTestSuite, HandleValidInputs)
+class DivFunTestSuite : public testing::TestWithParam<std::tuple<int, int, int>>
+{
+    protected:
+    DivFunTestSuite(){}
+    ~DivFunTestSuite(){}
+};
+
+TEST_P(DivFunTestSuite, HandleValidInputs) 
+{
+    int numerator = std::get<0>(GetParam());
+    int denominator = std::get<1>(GetParam());
+    int expected_value = std::get<2>(GetParam());
+
+    int actual_value = divFun(numerator, denominator);
+
+    ASSERT_EQ(actual_value, expected_value);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+        DivFunTestSuiteParameterExample,
+        DivFunTestSuite,
+        ::testing::Values(
+                std::make_tuple(10, 5, 2),
+                std::make_tuple(10, 0, 0),
+                std::make_tuple(10, -5, 0)));
+
+/*TEST(DivFunTestSuite, HandleValidInputs)
 {
     int numerator = 10;
     int denominator = 5;
@@ -41,4 +67,4 @@ TEST(DivFunTestSuite, HandleNegativeDenominator)
     int actual_value = divFun(numerator, denominator);
 
     ASSERT_EQ(actual_value, expected_value);
-}
+}*/

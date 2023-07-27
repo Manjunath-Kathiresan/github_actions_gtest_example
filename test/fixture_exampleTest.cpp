19a920e4d98688751c1d3c954020545aba7c5b51
jkm1cob <kathiresan.manjunath@in.bosch.com>
2023-07-27 11:44:05 +0530

    create test fixture

--- test/fixture_exampleTest.cpp
+++ test/fixture_exampleTest.cpp
@@ -19,7 +19,60 @@ class A {
     }
 };
 
-TEST(TestFixtureTestSuite, OperationTrueTest)
+// Test fixture class which is inherited from google's test class
+class TestFixtureTestSuite : public testing::Test
+{
+    // Anything we write in this class should be under protected
+    protected:
+    A *aPtr;
+    B *bPtr;
+    C *cPtr;
+
+    TestFixtureTestSuite()
+    {
+        // initialization
+    }
+
+    ~TestFixtureTestSuite
+    {
+        // releasing the resources
+    }
+
+    // Alternate option instead of constructor and destructor, we can use SetUp and TearDown provided by google and override them
+    // SetUp() is a API provided by google's test class
+    void SetUp()
+    {
+        this->cPtr = new C();
+        this->bPtr = new B(cPtr);
+        this->aPtr = new A(bPtr);
+    }
+
+    // TearDown() is a API provided by google's test class
+    void TearDown()
+    {
+        delete cPtr;
+        delete bPtr;
+        delete aPtr;
+    }
+}
+
+TEST_F(TestFixtureTestSuite, OperationTrueTest)
+{
+    // Arrange
+    // Given by the fixture class TestFixtureTestSuite
+    // Act and Assert
+    ASSERT_TRUE(aPtr->operation());
+}
+
+TEST_F(TestFixtureTestSuite, OperationTrueTest)
+{
+    // Arrange
+    // Given by the fixture class TestFixtureTestSuite
+    // Act and Assert
+    ASSERT_FALSE(aPtr->operation());
+}
+
+/*TEST(TestFixtureTestSuite, OperationTrueTest)
 {
     // Arrange
     C cObj;
@@ -39,5 +92,5 @@ TEST(TestFixtureTestSuite, OperationFalseTest)
 
     // Act and Assert
     ASSERT_FALSE(aObj.operation());
-}
+}*/
 
